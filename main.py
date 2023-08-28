cae380667e5106de1d7d218ae77cb6f43f3899ce
kinanhino <kinan.hino@e.braude.ac.il>
2023-08-29 01:27:29 +0300

    added some comments and changed part of the names to make it more understandable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# a function that checks if a giving string is a valid IPv4 octet.
+# a function that checks if a giving string is a valid IPv4 octet, 0-255 integer value
 def check_ip4_octet(octet):
     try:
         # check if the octet is an integer
@@ -14,9 +14,13 @@ def check_ip4_octet(octet):
 
 
 # function to check for v4 validity
+# takes the ip_address as a raw string
 def check_for_IPv4(ip_addr):
+    # ipv4 is represented by x.x.x.x where x is 0-255
     ip = ip_addr.split(".")
+    #ipv4 must have 4 octets
     if len(ip) == 4:
+        # check each octet separately
         for octet in ip:
             if not check_ip4_octet(octet):
                 return False
@@ -25,17 +29,22 @@ def check_for_IPv4(ip_addr):
     return False
 
 
-def check_for_IPv6(ip_addr):
+# function to check for v6 validity
+# takes the ip_adress as a list seperated by ':' colons
+def normal_or_dual_v6(ip_addr):
+    # cut last hextet for ipv4 search
     last_hextet = ip_addr[-1]
     ip = ip_addr[:-1]
     if len(ip) < 8:
         for hextet in ip:
             # print(f"'{hextet}'")
+            # check if a hextet is not a valid hextet and not empty (zeros) then it's not an ipv6 address
             if not check_ip6_hextet(hextet) and hextet != "":
                 return "Neither"
-        # a dual ip must be y:y:y:y:y:y:x.x.x.x ,where x must be between 0-255 and y neither empty or 0000-ffff
+        # last hextet (after last ':' colon) is actually an ipv4 address
         if check_for_IPv4(last_hextet) and len(ip) < 7:
             return "dual"
+        # last hextet is a regular hextet
         elif check_ip6_hextet(last_hextet) or last_hextet == "":
             return "ipv6"
         else:
@@ -44,6 +53,7 @@ def check_for_IPv6(ip_addr):
         return "Neither"
 
 
+# function to check if a given string is a valid hextet value
 def check_ip6_hextet(hextet):
     valid_characters = "0123456789abcdef"
     if len(hextet) < 5:
@@ -54,19 +64,20 @@ def check_ip6_hextet(hextet):
     return False
 
 
-def which_version(ip_addr):
-    normal_or_dual = check_for_IPv6(ip_addr)
+def which_ipv6_version(ip_addr):
+    normal_ipv6_dual = normal_or_dual_v6(ip_addr)
     ip_addr = ":".join(ip_addr)
-    if normal_or_dual == "ipv6":
+    if normal_ipv6_dual == "ipv6":
         return f"{ip_addr} is a Valid IPv6 address"
-    elif normal_or_dual == "dual":
+    elif normal_ipv6_dual == "dual":
         return f"{ip_addr} is a Valid dual ip address"
-    elif normal_or_dual == "Neither":
+    elif normal_ipv6_dual == "Neither":
         return f"{ip_addr} is Not a Valid ip address"
     else:
         return f"something is very wrong with that {ip_addr}"
 
 
+# main function that checks if its a valid ip at all and calling the right function to get the version type
 def check_valid_ip(ip_addr):
     ipv6 = ip_addr.split(":")
     ipv4 = ip_addr.split(".")
@@ -79,7 +90,7 @@ def check_valid_ip(ip_addr):
         # must be an inner else!!
     else:
         # it must be an ipv6
-        return which_version(ipv6)
+        return which_ipv6_version(ipv6)
 
 
 if __name__ == '__main__':
